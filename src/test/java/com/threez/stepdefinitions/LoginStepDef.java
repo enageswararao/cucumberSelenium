b99f373a02e38ea1e9acb6cb15e350c6bca7bb27
Nageswara Rao <enageswararao@gmail.com>
2020-06-14 21:24:09 +0530

    Update LoginStepDef.java
    import io.github.bonigarcia.wdm.WebDriverManager;

--- src/test/java/com/threez/stepdefinitions/LoginStepDef.java
+++ src/test/java/com/threez/stepdefinitions/LoginStepDef.java
@@ -5,7 +5,7 @@ import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import com.hrmapp.pages.LoginPage;
 
 import cucumber.api.java.After;
